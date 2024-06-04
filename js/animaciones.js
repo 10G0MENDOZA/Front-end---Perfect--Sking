c26c61f7fa4b85737a36f3e3a7a549f1b602f618
10GOMENDOZA <tmeza5839@gmail.com>
2024-06-04 16:51:12 -0500

    cambios en el login

---
+++ js/animaciones.js
@@ -0,0 +1,17 @@
+/*ESPERA QUE CARGUE LOS ELEMENTOS DE TODA LA PANTALLA */
+
+/*LOGO*/
+window.onload = function(){
+    var img = document.getElementById('imagentecno');
+}
+/* DESCRIPCION Y LOS BOTONES*/
+window.onload = function(){
+    var h4 = document.getElementById('desgeneral');
+    var h5 = document.getElementById('desgeneral');
+    var img = document.getElementById('desgeneral');
+    var div = document.getElementById('desgeneral');
+}
+
+window.onload = function(){
+    var div = document.getElementById('informacion');
+}
